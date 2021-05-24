c8f58057cf0989b106c38e94218841329ded3faa
Andrelino Silva <andrelinocom@gmail.com>
2021-05-24 07:55:00 -0300

    Continuando implementação do deseafio

--- src/components/Comments/index.tsx
+++ src/components/Comments/index.tsx
@@ -3,7 +3,7 @@ import { useEffect } from 'react';
 export default function Comments(): JSX.Element {
   useEffect(() => {
     const script = document.createElement('script');
-    const anchor = document.getElementById('inject-comments-for-uterances');
+    const anchor = document.getElementById('inject-comments-for-utterances');
     script.setAttribute('src', 'https://utteranc.es/client.js');
     script.setAttribute('crossorigin', 'anonymous');
     script.setAttribute(
@@ -11,10 +11,11 @@ export default function Comments(): JSX.Element {
       'andrelinos/ignite-reactjs-criando-um-projeto-do-zero'
     );
     script.setAttribute('issue-term', 'pathname');
+    script.setAttribute('label', 'Comment :speech_balloon:');
     script.setAttribute('theme', 'github-dark');
     script.setAttribute('async', 'async');
     anchor.appendChild(script);
   }, []);
 
-  return <div id="inject-comments-for-uterances" />;
+  return <div id="inject-comments-for-utterances" />;
 }
